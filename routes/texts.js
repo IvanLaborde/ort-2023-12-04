0c18a080db94b818138a74754efb0d275f327e84
IvanLaborde <labordeboy2000@gmail.com>
2023-12-04 21:24:21 -0300

    Quito comentarios

--- routes/texts.js
+++ routes/texts.js
@@ -8,8 +8,8 @@ router.get('/:id', async function(req, res, next) {
         res.status(200).json(texto)
     }
     catch(e){
-        console.log(e.message) // Log the error message
-        res.status(500).json({message: "error", error: e.message}) // Send the error message in the response
+        console.log(e)
+        res.status(500).json({message: "error"})
     }
 });
 
